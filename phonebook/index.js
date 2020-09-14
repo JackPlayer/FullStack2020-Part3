cdf6474d5902083972b5b10514a693835cc9e588
Jack Player <jackson.m.player@gmail.com>
2020-09-14 13:16:22 -0700

    3.7: Added morgan middleware

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 let persons = [
@@ -28,6 +29,7 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 
 const generateId = () => {
