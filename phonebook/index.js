aed9db4aee1086b11f4ad4ad3584cfffdf0e3aab
Jack Player <jackson.m.player@gmail.com>
2020-09-16 15:44:09 -0700

    3.22: Added lint configuration and fixed linting issues

--- phonebook/index.js
+++ phonebook/index.js
@@ -2,143 +2,126 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const app = express()
 const Person = require('./models/person')
+
+const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 app.use(morgan((tokens, req, res) => {
-    const data = (tokens.method(req, res) === "POST") ? JSON.stringify(req.body) : ''
-    return [
-        tokens.method(req,res),
-        tokens.url(req,res),
-        tokens.status(req,res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        data
-        
-    ].join(' ')
+  const data = (tokens.method(req, res) === 'POST') ? JSON.stringify(req.body) : ''
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    data,
+  ].join(' ')
 }))
 
-const persons = [
-
-]
-
 // Info Page
-app.get('/info', (request, response) =>{
-    const date = new Date()
-    let info = '<p>No info available</p>'
-    Person
-        .find({})
-        .then((persons) => {
-            info = 
-            `
+app.get('/info', (request, response) => {
+  const date = new Date()
+  let info = '<p>No info available</p>'
+  Person
+    .find({})
+    .then((persons) => {
+      info = `
             <p>Phonebook has info for ${persons.length} people</p>
             <p>${date}</p>
             `
-            response.send(info)
-        })
-        .catch((error) => {
-            console.error(error)
-            response.send(info)
-        })
+      response.send(info)
     })
+    .catch(() => {
+      response.send(info)
+    })
+})
 
 // README Page
 app.get('/readme', (request, response) => {
-    response.sendFile('./README.md', { root: __dirname})
-
+  response.sendFile('./README.md', { root: __dirname })
 })
 
 // Get full collection
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then((persons) => {
-        response.json(persons)
-    })
-    .catch(error => next(error))
-    
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
+    .catch((error) => next(error))
 })
 
 // Get individual resource
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    
-    Person.findById(id).then((person) => {
-        response.json(person)
-    })
-    .catch((error) => next(error))
+  const { id } = request.params
 
+  Person.findById(id).then((person) => {
+    response.json(person)
+  })
+    .catch((error) => next(error))
 })
 
 // Delete entry
 app.delete('/api/persons/:id', (request, response, next) => {
-    console.log("Requested")
-    Person.findByIdAndRemove(request.params.id)
-        .then((result) => {
-            response.status(204).end()
-        })
-        .catch(error => {
-            next(error)
-        })
-
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 // Add entry
 app.post('/api/persons', (request, response, next) => {
+  const { body } = request
 
-    const body = request.body
+  const newEntry = new Person({
+    date: new Date(),
+    name: body.name,
+    number: body.number,
+  })
 
-    const newEntry = new Person({
-        date: new Date(),
-        name: body.name,
-        number: body.number
-    })
-
-    newEntry.save().then(savedPerson => {
-        response.json(savedPerson.toJSON())
-    })
-    .catch(error => next(error))
-    
+  newEntry.save().then((savedPerson) => {
+    response.json(savedPerson.toJSON())
+  })
+    .catch((error) => next(error))
 })
 
 // Update entry
 app.put(('/api/persons/:id'), (request, response, next) => {
-    const body = request.body
+  const { body } = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    
-    Person.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true})
-        .then((updatedPerson) => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
 })
 
-
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-
-    if(error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
-    }else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-    next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  return next(error)
 }
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const { PORT } = process.env
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- phonebook/models/person.js
+++ phonebook/models/person.js
@@ -1,50 +1,53 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
 
-
-require('dotenv').config({path: '../.env'})
+require('dotenv').config({ path: '../.env' })
 
 const url = process.env.MONGO
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then((response) => {
-        console.log('Connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log("Error connecting to MongoDB")
-    })
-    
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((err) => {
+    console.log(err)
+    console.log('Error connecting to MongoDB')
+  })
+
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
-        unique: true
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        required: true,
-    },
-    date: {
-        type: Date, 
-        required: true,
-    },
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
 })
 
 personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
 })
 
 module.exports = mongoose.model('Person', personSchema)
